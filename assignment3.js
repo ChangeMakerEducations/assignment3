49374296e648394e06fe7a4f75ea8401bae73244
elinlindmark <56250080+elinlindmark@users.noreply.github.com>
2019-12-09 10:10:03 +0100

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,30 @@
+function DisplayInputInOutout() {
+	let input = document.getElementById('numberInput').value;
+
+	console.log(input);
+	if(input){
+		document.getElementById('outputBox').innerHTML = input;
+	}
+}
+
+function DisplayInputInIn() {
+	let input = document
+	.getElementById('numberInput2')
+	.value;
+
+	if(input); {
+		document.getElementById('output2').value=input;
+	}
+}
+
+function DisplayOutout() {
+	let input = document
+	.getElementById('numberInput3')
+	.innerHTML;
+
+	if(input) {
+		document.getElementById('numberInput3').innerHTML;
+	}
+
+	else if (alert('Oooooopzzz!')); 
+}
